1ee747b39c093334820ee63a6578e60097eb16bf
DJ-Joel <joel.hemphill@gmail.com>
2026-07-31 22:58:06 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ siteheader("Home");
 navbar("index.php");
 
 searchform();
+
+if (isLoggedIn())
+{
+	echo "<p class=info>Message the KJ if you couldn't find a song you wanted in the songbook. <a href=\"chat.php\">Message the KJ</a></p>";
+}
+else
+{
+	echo "<p class=info>Message the KJ if you couldn't find a song you wanted in the songbook. (Account Required)<br><a href=\"login.php?redirect=chat.php\">Log in</a> or <a href=\"register.php\">create a free account</a></p>";
+}
 /*
 if ($screensize == 'xlarge')
 {
